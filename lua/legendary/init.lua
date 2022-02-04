e40c86c950ed3b610ebbc291bf0c9672628d5ddb
Mat Jones <mat@mjones.network>
2022-02-04 13:11:25 -0500

    add check for required APIs before continuing

--- lua/legendary/init.lua
+++ lua/legendary/init.lua
@@ -1,5 +1,10 @@
 local M = {}
 
+if not vim.keymap or not vim.keymap.set then
+  vim.api.nvim_err_write('Sorry, legendary.nvim requires Neovim 0.7.0 or higher!')
+  return
+end
+
 M.bind = require('legendary.bindings').bind
 M.find = require('legendary.bindings').find
 
