8075adbb297d966c575126696d6da1c2ce8f83b9
Mat Jones <mat@mjones.network>
2022-02-12 16:08:34 -0500

    implement which-key.nvim integration
    Former-commit-id: 03c1577f73c75ef3ee523d7ca5ff88f121a82444

---
+++ lua/legendary/compat/which-key.lua
@@ -0,0 +1,50 @@
+local M = {}
+
+local function wk_to_legendary(wk)
+  local legendary = {}
+  legendary[1] = wk.prefix
+  if wk.cmd then
+    legendary[2] = wk.cmd
+  end
+  legendary.description = wk.label
+  legendary.opts = wk.opts or {}
+  return legendary
+end
+
+function M.parse_whichkey(which_key_tbls, which_key_opts)
+  local wk_parsed = require('which-key.keys').parse_mappings({}, which_key_tbls, which_key_opts.prefix or '')
+  local legendary_tbls = {}
+  for _, wk in pairs(wk_parsed) do
+    -- check wk.group because these don't represent standalone keymaps
+    -- they basically represent a "folder" of other keymaps
+    -- TODO support which-key mappings with buf values
+    if not wk.label or wk.group or wk.buf then
+      goto continue
+    end
+
+    table.insert(legendary_tbls, wk_to_legendary(wk))
+
+    ::continue::
+  end
+  return legendary_tbls
+end
+
+function M.bind_whichkey(wk_tbls, wk_opts)
+  local legendary_tbls = M.parse_whichkey(wk_tbls, wk_opts)
+  require('legendary').bind_keymaps(legendary_tbls)
+end
+
+--- Enable auto-registering of which-key.nvim tables
+--- with legendary.nvim
+function M.whichkey_listen()
+  local wk = require('which-key')
+  local original = wk.register
+  local listener = function(whichkey_tbls, whichkey_opts)
+    M.bind_whichkey(whichkey_tbls, whichkey_opts)
+    original(whichkey_tbls, whichkey_opts)
+  end
+  wk.register = listener
+  return true
+end
+
+return M
--- lua/legendary/init.lua
+++ lua/legendary/init.lua
@@ -1,17 +1,3 @@
-if not vim.keymap or not vim.keymap.set then
-  local function print_err()
-    vim.api.nvim_err_write('Sorry, legendary.nvim requires Neovim 0.7.0 or higher!')
-  end
-
-  print_err()
-
-  return {
-    bind = print_err,
-    find = print_err,
-    setup = print_err,
-  }
-end
-
 local M = {}
 
 function M.setup(new_config)
@@ -38,8 +24,27 @@ function M.setup(new_config)
   if config.commands and #config.commands > 0 then
     require('legendary').bind_commands(config.commands)
   end
+
+  if config.auto_register_which_key then
+    local whichkey_is_loaded, _ = pcall(require, 'which-key')
+    if whichkey_is_loaded then
+      require('legendary.compat.which-key').whichkey_listen()
+    end
+  end
 end
 
+M.bind_whichkey = require('legendary.compat.which-key').bind_whichkey
 M = vim.tbl_extend('error', M, require('legendary.bindings'))
+
+if not vim.keymap or not vim.keymap.set then
+  local function print_err()
+    vim.api.nvim_err_write('Sorry, legendary.nvim requires Neovim 0.7.0 or higher!')
+  end
+
+  print_err()
+  for key, _ in pairs(M) do
+    M[key] = print_err
+  end
+end
 
 return M
