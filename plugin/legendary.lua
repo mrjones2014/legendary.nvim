f71173c51df19d99ba47c5bd77454cc9b6cd0065
Mat Jones <mat@mjones.network>
2022-02-13 12:14:07 -0500

    Implement ability to search only keymaps or only commands
    resolves #10

--- plugin/legendary.lua
+++ plugin/legendary.lua
@@ -3,14 +3,42 @@ if not vim.keymap or not vim.keymap.set then
   return
 end
 
-local function find()
+local function trim(s)
+  return (string.gsub(s, '^%s*(.-)%s*$', '%1'))
+end
+
+local function find(opts)
+  if not opts or not opts.args then
+    return require('legendary').find()
+  end
+
+  if trim(opts.args:lower()) == 'keymaps' then
+    return require('legendary').find('keymaps')
+  end
+
+  if trim(opts.args:lower()) == 'commands' then
+    return require('legendary').find('commands')
+  end
+
   require('legendary').find()
 end
 
-vim.api.nvim_add_user_command('Legendary', find, {
-  desc = 'Find keymaps and commands with vim.ui.select()',
-})
+local function command_completion(arg_lead)
+  if arg_lead and trim(arg_lead):sub(1, 1):lower() == 'k' then
+    return { 'keymaps' }
+  end
 
-vim.api.nvim_add_user_command('Legend', find, {
+  if arg_lead and trim(arg_lead):sub(1, 1):lower() == 'c' then
+    return { 'commands' }
+  end
+
+  return { 'keymaps', 'commands' }
+end
+
+local opts = {
   desc = 'Find keymaps and commands with vim.ui.select()',
-})
+  nargs = 1,
+  complete = command_completion,
+}
+
+vim.api.nvim_add_user_command('Legendary', find, opts)
