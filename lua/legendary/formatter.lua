6a86326c8ce691c27da85d35b2acd79bc204119f
Mat Jones <mat@mjones.network>
2022-03-14 08:10:21 -0400

    add tests

--- lua/legendary/formatter.lua
+++ lua/legendary/formatter.lua
@@ -7,11 +7,11 @@ local function lpad(str, len)
 end
 
 local function col1_str(item)
-  if item.kind == 'legendary-command' then
+  if item.kind == 'legendary.command' then
     return '<cmd>'
   end
 
-  if item.kind == 'legendary-autocmd' then
+  if item.kind == 'legendary.autocmd' then
     local events = item[1]
     if type(events) == 'table' then
       events = table.concat(events, ', ')
@@ -29,7 +29,7 @@ local function col1_str(item)
 end
 
 local function col2_str(item)
-  if item.kind == 'legendary-autocmd' then
+  if item.kind == 'legendary.autocmd' then
     local patterns = item.opts and item.opts.pattern or '*'
     if type(patterns) == 'table' then
       patterns = table.concat(patterns, ', ')
@@ -45,6 +45,19 @@ local function col3_str(item)
   return item.description or ''
 end
 
+--- Default implementation of config.formatter
+--- Column one:
+---   - Keymaps => modes
+---   - Commands => '<cmd>'
+---   - Autocmds => events
+--- Column 2:
+---   - Keymaps => key codes
+---   - Commands => command
+---   - Autocmds => patterns
+--- Column 3:
+---   - All => description
+---@param item LegendaryItem
+---@return table the values to format
 function M.get_default_format_values(item)
   return {
     col1_str(item),
@@ -53,6 +66,11 @@ function M.get_default_format_values(item)
   }
 end
 
+--- Get the column values to be formatted
+--- for item. Uses config.formatter if not nil,
+--- default implementation otherwise.
+---@param item LegendaryItem
+---@return table the values to format
 function M.get_format_values(item)
   local formatter = require('legendary.config').formatter
   if formatter and type(formatter) == 'function' then
@@ -74,6 +92,8 @@ function M.get_format_values(item)
   return M.get_default_format_values(item)
 end
 
+--- Update cached column paddings.
+---@param item LegendaryItem
 function M.update_padding(item)
   local values = M.get_format_values(item)
   for i, value in pairs(values) do
@@ -83,8 +103,18 @@ function M.update_padding(item)
   end
 end
 
+--- Returns a READ-ONLY COPY of the current padding values.
+--- Note that this is a COPY of the table so it will not update,
+--- and writing to it will not do anything. Padding is managed
+--- internally by the formatter and should not be modified manually.
+---@return table a table containing the padding value for each column.
+function M.get_padding()
+  return vim.deepcopy(padding)
+end
+
 --- Format a LegendaryItem to a string
 ---@param item LegendaryItem
+---@return string
 function M.format(item)
   local values = M.get_format_values(item)
 
