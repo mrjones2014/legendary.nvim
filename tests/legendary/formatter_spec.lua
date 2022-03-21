aeff8514faf72a6bb6418d54e2dc9985a0487b73
Mat Jones <mat@mjones.network>
2022-03-21 07:50:46 -0400

    update padding computation to account for unicode characters
    Fixes: #96
    
    Former-commit-id: 35164c9b5c07d040f3407e86911f8fccd828180b

--- tests/legendary/formatter_spec.lua
+++ tests/legendary/formatter_spec.lua
@@ -39,6 +39,10 @@ describe('formatter', function()
   end)
 
   describe('update_padding(item)', function()
+    before_each(function()
+      formatter.__clear_padding()
+    end)
+
     it('sets padding to the length of the longest value in each column', function()
       require('legendary.config').formatter = function(item)
         return {
@@ -64,5 +68,75 @@ describe('formatter', function()
       assert(padding[2] == #'lua vim.lsp.buf.definition')
       assert(padding[3] == #'Go to definition with LSP')
     end)
+
+    it('computes length correctly when string contains unicode characters', function()
+      require('legendary.config').formatter = function(item)
+        return {
+          item[1],
+          item[2],
+          item.description,
+        }
+      end
+
+      local items = {
+        { '∆', '', description = '' },
+        { '˚', '', description = '' },
+        { 'ݑ', '', description = '' },
+      }
+
+      vim.tbl_map(function(item)
+        formatter.update_padding(item)
+      end, items)
+
+      local padding = formatter.get_padding()
+      assert(#padding == 1)
+      assert(padding[1] == 1)
+    end)
+  end)
+
+  describe('lpad(str)', function()
+    it(
+      'padds all strings to have the same length based on the padding table, accounting for unicode characters',
+      function()
+        require('legendary.config').formatter = function(item)
+          return {
+            item[1],
+            item[2],
+            item.description,
+          }
+        end
+
+        local items = {
+          { '<leader>c', ':CommentToggle<CR>', description = 'Toggle comment' },
+          { '<leader>s', ':wa<CR>', description = 'Write all buffers' },
+          { 'gd', 'lua vim.lsp.buf.definition', description = 'Go to definition with LSP' },
+          { '∆', ':echo "test"<CR>', description = 'Contains a triangle' },
+          { '˚', ':lua print("test")<CR>', description = 'Contains a unicode dot' },
+          { 'ݑ', ':e<CR>', description = 'Contains a unicode character' },
+        }
+
+        vim.tbl_map(function(item)
+          formatter.update_padding(item)
+        end, items)
+
+        local padded = {}
+        local padding = formatter.get_padding()
+        vim.tbl_map(function(item)
+          table.insert(padded, {
+            formatter.rpad(item[1], padding[1]),
+            formatter.rpad(item[2], padding[2]),
+            formatter.rpad(item[3], padding[3]),
+          })
+        end, items)
+
+        for i, _ in pairs(padded) do
+          if i < #padded then
+            assert(formatter.utf8_len(padded[i][1]) == formatter.utf8_len(padded[i + 1][1]))
+            assert(formatter.utf8_len(padded[i][2]) == formatter.utf8_len(padded[i + 1][2]))
+            assert(formatter.utf8_len(padded[i][3]) == formatter.utf8_len(padded[i + 1][3]))
+          end
+        end
+      end
+    )
   end)
 end)
