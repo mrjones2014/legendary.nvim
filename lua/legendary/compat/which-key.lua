cb43d4a16351117361ed684d4248c3b393a6c60b
Mat Jones <mat@mjones.network>
2022-04-19 10:40:52 -0400

    Use package.searchers to make which-key integration not dependent on load order
    Former-commit-id: 797a4432d39282f719c14b2c0d70431b4cf4c7a9

--- lua/legendary/compat/which-key.lua
+++ lua/legendary/compat/which-key.lua
@@ -1,4 +1,4 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local table = _tl_compat and _tl_compat.table or table; require('legendary.types')
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local package = _tl_compat and _tl_compat.package or package; local pcall = _tl_compat and _tl_compat.pcall or pcall; local table = _tl_compat and _tl_compat.table or table; require('legendary.types')
 local M = {}
 
 local function wk_to_legendary(wk, wk_opts)
@@ -65,15 +65,33 @@ end
 
 
 
-function M.whichkey_listen()
-   local wk = (_G['require']('which-key'))
-   local original = wk.register
-   local listener = function(whichkey_tbls, whichkey_opts)
-      M.bind_whichkey(whichkey_tbls, whichkey_opts, false)
-      original(whichkey_tbls, whichkey_opts)
+function M.whichkey_listen(halt)
+   local loaded, which_key = pcall(
+   _G['require'],
+   'which-key')
+
+
+   if loaded then
+      local wk = which_key
+      local original = wk.register
+      local listener = function(whichkey_tbls, whichkey_opts)
+         M.bind_whichkey(whichkey_tbls, whichkey_opts, false)
+         original(whichkey_tbls, whichkey_opts)
+      end
+      wk.register = listener
+      return true
+   elseif not halt then
+
+
+      local searcher = function(module)
+         if module == 'which-key' then
+            M.whichkey_listen(true)
+         end
+
+         return nil
+      end
+      table.insert(package.searchers, 1, searcher)
    end
-   wk.register = listener
-   return true
 end
 
 return M
