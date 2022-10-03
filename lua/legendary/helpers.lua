8ea9e2f3de71e41d198ce6aad8c8fe21f86b01f8
Mat Jones <mat@mjones.network>
2022-10-03 09:19:33 -0400

    support dots to access functions nested in tables in lazy_required_fn helper

--- lua/legendary/helpers.lua
+++ lua/legendary/helpers.lua
@@ -1,4 +1,4 @@
-
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local string = _tl_compat and _tl_compat.string or string
 
 require('legendary.types')
 local M = {}
@@ -15,6 +15,19 @@ function M.lazy(fn, ...)
    end
 end
 
+local function is_function(a)
+   if type(a) == 'function' then
+      return true
+   end
+
+   local mt = getmetatable(a)
+   if not mt then
+      return false
+   end
+
+   return not not mt.__call
+end
+
 
 
 
@@ -24,7 +37,26 @@ end
 function M.lazy_required_fn(module_name, fn_name, ...)
    local args = { ... }
    return function()
-      ((_G['require'](module_name))[fn_name])(unpack(args))
+      local module = (_G['require'](module_name))
+      if string.find(fn_name, '%.') then
+         local fn = module
+         for _, key in ipairs(vim.split(fn_name, '%.')) do
+            fn = (fn)[key]
+            if fn == nil then
+               vim.notify('[legendary.nvim]: invalid lazy_required_fn usage: no such function path')
+               return
+            end
+         end
+         if not is_function(fn) then
+            vim.notify('[legendary.nvim]: invalid lazy_required_fn usage: no such function path')
+            return
+         end
+         local final_fn = fn
+         final_fn(unpack(args))
+      else
+         local fn = module[fn_name]
+         fn(unpack(args))
+      end
    end
 end
 
