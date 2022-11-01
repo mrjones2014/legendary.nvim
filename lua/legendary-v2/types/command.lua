adecb19e5f1e4a5e0bf8da66a7163200c9e11e58
Mat Jones <mat@mjones.network>
2022-11-01 18:36:53 -0400

    add command test
    Former-commit-id: 91869df245954a711a77b9fb8832313ba02a7fd3

--- lua/legendary-v2/types/command.lua
+++ lua/legendary-v2/types/command.lua
@@ -2,6 +2,23 @@ local class = require('legendary-v2.middleclass')
 local Id = require('legendary-v2.id')
 local util = require('legendary-v2.util')
 
+local function sanitize_cmd_name(cmd_orig)
+  local cmd = (cmd_orig .. ''):gsub('{.*}$', ''):gsub('%[.*%]$', '')
+  if vim.startswith(cmd:lower(), '<cmd>') then
+    cmd = cmd:sub(6)
+  elseif vim.startswith(cmd, ':') then
+    cmd = cmd:sub(2)
+  end
+
+  if vim.endswith(cmd:lower(), '<cr>') then
+    cmd = cmd:sub(1, #cmd - 4)
+  elseif vim.endswith(cmd, '\r') then
+    cmd = cmd:sub(1, #cmd - 2)
+  end
+
+  return vim.trim(cmd)
+end
+
 ---@class Command
 ---@field cmd string
 ---@field implementation string|function|nil
@@ -11,6 +28,9 @@ local util = require('legendary-v2.util')
 ---@field id integer
 local Command = class('Command')
 
+---Parse a command table
+---@param tbl table
+---@return Command
 function Command:parse(tbl)
   vim.validate({
     ['1'] = { tbl[1], { 'string' } },
@@ -34,12 +54,21 @@ function Command:parse(tbl)
   return self
 end
 
+---Create the user command
+---@return Command
 function Command:apply()
   if not self.implementation then
-    return
+    return self
   end
 
   local opts = vim.deepcopy(self.opts or {})
   opts.desc = opts.desc or self.description
-  vim.api.nvim_create_user_command(self.cmd, self.implementation, opts)
+
+  -- replace any argument placeholders for display purposes wrapped in {} or []
+  -- % is escape character in gsub patterns
+  -- strip param names between [] or {}
+  vim.api.nvim_create_user_command(sanitize_cmd_name(self.cmd), self.implementation, opts)
+  return self
 end
+
+return Command
