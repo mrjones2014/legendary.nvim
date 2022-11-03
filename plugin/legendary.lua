e1989c3470faff99e008b7bf87917231848ad280
Mat Jones <mat@mjones.network>
2022-11-03 12:31:52 -0400

    add notice about versioning
    Former-commit-id: b91c6f6d9d933b97e47c2447773677d4f0cac2e1

--- plugin/legendary.lua
+++ plugin/legendary.lua
@@ -2,8 +2,6 @@ if vim.g.legendary_loaded then
   return
 end
 
-vim.g.legendary_root_dir = vim.fn.expand('<sfile>:p:h:h')
-
 if not vim.keymap or not vim.keymap.set then
   vim.notify('Sorry, legendary.nvim requires Neovim 0.7.0 or higher!', vim.log.levels.ERROR)
   return
