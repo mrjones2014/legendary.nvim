efc0b7dda55281361b859c334dcb73cf2c42f039
figsoda <figsoda@pm.me>
2023-01-05 13:14:29 -0500

    fix: duplicate help tags

--- vimdoc-gen.lua
+++ vimdoc-gen.lua
@@ -16,7 +16,7 @@ for input, output in pairs(DOC_FILES) do
   require('ts-vimdoc').docgen({
     input_file = input,
     output_file = output,
-    project_name = 'legendary',
+    project_name = output:match('/([^/]+)%.txt$'),
   })
   print(string.format('Wrote %s from source file %s', output, input))
 end
